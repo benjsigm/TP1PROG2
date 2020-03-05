62e1e9a88ff0fb3e7620be3564ad7cab5918e4a0
benjsigm <benjsigm@hotmail.com>
2020-03-04 21:55:41 -0500

    test

--- src/com/company/Dessin.java
+++ src/com/company/Dessin.java
@@ -24,5 +24,6 @@ public class Dessin {
         this.car = car;
     }
 
+    //Ajouter, Dessiner
 
 }
---
+++ src/com/company/Toile.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public abstract class Toile {
+
+    //Attributs
+    protected int x;//coin sup gauche
+    protected int y;//--"--
+
+
+}
